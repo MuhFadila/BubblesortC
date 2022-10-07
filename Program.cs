da2c5184d590f80ca865f0042f95b6e43bccb499
MuhFadila <muhammad.fadila.ft21@mail.umy.ac.id>
2022-10-07 17:12:58 +0700

    Deklarasi array int dengan ukuran 20 Deklarasi variabel int untuk menyimpan banyaknya data pada array Fungsi/Method untuk menerima masukan

--- Program.cs
+++ Program.cs
@@ -4,11 +4,16 @@ namespace BubblesortC
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        //Deklarasi variabel int untuk menyimpan banyaknya data pada array 
+        private int n;
+
+        // Fungsi/Method untuk menerima masukan
+        public void read()
+
+    
         }
     }
 }
